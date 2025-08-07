26c4fd3f49e5d6426a975ffd1125c0f05c1f7574
sacilingir <seyitacilingir@gmail.com>
2025-08-07 14:16:02 +0300

    message

---
+++ Infastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,35 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Concretes;
+using ETicaretAPI.Persistence.Contexts;
+using ETicaretAPI.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    //extension fonksiyonlar statictir.
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services) {
+
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+
+            services.AddScoped<ICustomerReadRepository,CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository,CustomerWriteRepository>();
+            services.AddScoped<IOrderWriteRepository,OrderWriteRepository>();
+            services.AddScoped<IOrderReadRepository,OrderReadRepository>();
+            services.AddScoped<IProductWriteRepository,ProductWriteRepository>();
+            services.AddScoped<IProductReadRepository,ProductReadRepository>();
+
+
+         
+        }
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -0,0 +1,25 @@
+using ETicaretAPI.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddPersistenceServices();
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
